32d450526adb11c76008cea2d786849080fd699f
vonhouser <tajcarson@gmail.com>
2015-06-21 14:42:15 -0400

    All photos entered.

--- js/main.js
+++ js/main.js
@@ -29,57 +29,57 @@ $(function () {
                 var value = this.x;
                 
                 switch (value) {
-                    case ('0'):
+                    case ('0 Meters'):
                         console.log('equals 1500', value);
-                        return '<img src="/img/vehicle.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/Grumpy.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                     case ('500'):
                         console.log('equals 500', value);
-                        return '<img src="/img/brick.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/DoublewithRany.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                      case ('1000'):
                         console.log('equals 1500', value);
-                        return '<img src="/img/vehicle.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/Quaker.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                     case ('1500'):
                         console.log('equals 1500', value);
-                        return '<img src="/img/vehicle.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/Quaker.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                     case ('2000'):
                         console.log('equals 1500', value);
-                        return '<img src="/img/vehicle.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/TajandStacyRow.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                     case ('3500'):
                         console.log('equals 1500', value);
-                        return '<img src="/img/vehicle.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/RegattaAfter.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                     case ('4000'):
                         console.log('equals 1500', value);
-                        return '<img src="/img/vehicle.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/Quaker.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                     case ('5500'):
                         console.log('equals 1500', value);
-                        return '<img src="/img/vehicle.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/TajandMeganQuaker.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                         case ('7000'):
                         console.log('equals 1500', value);
-                        return '<img src="/img/vehicle.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/RegattaAfter.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                         case ('7500'):
                         console.log('equals 1500', value);
-                        return '<img src="/img/vehicle.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/MeanFace.jpg.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                         case ('8000'):
                         console.log('equals 1500', value);
-                        return '<img src="/img/vehicle.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/StupidSprint.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                         case ('8500'):
                         console.log('equals 1500', value);
-                        return '<img src="/img/vehicle.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/StupidSprint.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                         case ('9000'):
                         console.log('equals 1500', value);
-                        return '<img src="/img/vehicle.jpg" title="" alt="" border="0" height="250" width="220">';
+                        return '<img src="/img/SunsetRow8.jpg" title="" alt="" border="0" height="250" width="220">';
                         break;
                     default: 
                         console.log(value);
